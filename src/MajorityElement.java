ed727f050c5225ab0ecb832c4a33f36dc88e6e68
dishanksanghavi <dishanks1997@gmail.com>
2025-03-06 16:48:40 -0500

    Leetcode 169 : Majority Element

---
+++ src/MajorityElement.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+        public static int majorityElement(int[] nums) {
+            int len = nums.length;
+            Map<Integer, Integer> counts = new HashMap<>();
+            for (int num : nums) {
+                counts.compute(num, (key, oldValue) -> oldValue == null ? 1 : oldValue + 1);
+                if (counts.get(num) > len/2)
+                    return num;
+            }
+            return 0;
+        }
+
+    public static void main(String[] args) {
+        int[] nums1 = {3,2,3};
+        int result1 = majorityElement(nums1);
+        System.out.println("Majority Element : "+ result1);
+    }
+}
