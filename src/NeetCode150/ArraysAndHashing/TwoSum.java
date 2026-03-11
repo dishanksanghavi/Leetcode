8e5971552b557d6cd6cd7d6e54b6ea565568de9c
dishanksanghavi <dishanks1997@gmail.com>
2026-03-10 22:56:14 -0400

    Valid parentheses

--- src/NeetCode150/ArraysAndHashing/TwoSum.java
+++ src/NeetCode150/ArraysAndHashing/TwoSum.java
@@ -3,23 +3,23 @@ package NeetCode150.ArraysAndHashing;
 import java.util.*;
 
 public class TwoSum {
-    public static List<Integer> twoSum(int[] nums, int target){
+    public static int[] twoSum(int[] nums, int target){
         HashMap<Integer,Integer> seen = new HashMap<>();
         for (int i = 0; i<nums.length; i++ ){
             int compliment = target - nums[i];
             if (seen.containsKey(compliment)){
-                return Arrays.asList(seen.get(compliment),i);
+                return new int[]{seen.get(compliment), i};
             }
             seen.put(nums[i],i);
         }
-        return new ArrayList<>();
+        return new int[] {};
     }
     public static void main(String[] args) {
         int[] nums1 = {2,7,11,15};
         int target = 9;
         System.out.println("Original array: " + Arrays.toString(nums1) + " target sum : " + target);
-        List<Integer> result1 = twoSum(nums1, target);
-        System.out.println("Output : "+ result1);
+        int[] result1 = twoSum(nums1, target);
+        System.out.println("Output : "+ Arrays.toString(result1));
         System.out.println("Time Complexity: O(n)");
         System.out.println("Space Complexity: O(n)");
 
---
+++ src/NeetCode150/Stack/ValidParentheses.java
@@ -0,0 +1,38 @@
+package NeetCode150.Stack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> closeToOpen = new HashMap<>();
+        closeToOpen.put(')', '(');
+        closeToOpen.put(']', '[');
+        closeToOpen.put('}', '{');
+
+        for (char c : s.toCharArray()) {
+            if (closeToOpen.containsKey(c)) {
+                if (!stack.isEmpty() && stack.peek() == closeToOpen.get(c)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String str = "[(])";
+        System.out.println("Original String : " + str);
+        boolean result = isValid(str);
+        System.out.println("Is Valid Parentheses : "+ result);
+        System.out.println("Time complexity: O(n)");
+        System.out.println("Space Complexity: O(n)");
+    }
+}
